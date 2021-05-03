b4057450d28677bac7c96af6afc4232cc05f763f
nuko365 <nuko365@gmail.com>
2021-05-03 16:41:22 +0900

    13

---
+++ Chapter2/_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(0 == 0);
+            Console.WriteLine(0 == 1);
+            Console.WriteLine('a' == 'a');
+            Console.WriteLine('a' == 'b');
+            Console.WriteLine("hello" == "hello");
+            Console.WriteLine("hello" == "こんにちは");
+        }
+    }
+}
