8ba5791964b15361d2e365ec2b70539c290cc622
nuko365 <nuko365@gmail.com>
2021-05-03 23:21:11 +0900

    14

---
+++ Chapter2/_14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameA = new FullName("masanobu", "naruse");
+            var nameB = new FullName("masanobu", "naruse");
+
+            Console.WriteLine(nameA.Equals(nameB));
+        }
+    }
+}
