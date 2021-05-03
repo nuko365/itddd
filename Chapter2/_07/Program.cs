436d1c1b6853a6d22e81cedac98b74a4f58fe24f
nuko365 <nuko365@gmail.com>
2021-05-03 15:14:55 +0900

    07

---
+++ Chapter2/_07/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greet = "こんにちは";
+            Console.WriteLine(greet);
+            greet = "Hello";
+            Console.WriteLine(greet);
+        }
+    }
+}
