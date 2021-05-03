f698ae3699f2b035f4161bc3a5b285a4425fb779
nuko365 <nuko365@gmail.com>
2021-05-03 12:22:02 +0900

    01

---
+++ Chapter2/_01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "naruse masanobu";
+            Console.WriteLine(fullName);
+        }
+    }
+}
