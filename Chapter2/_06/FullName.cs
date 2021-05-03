b88d6edd35eee9202bb27d452f8f0a705272fbd1
nuko365 <nuko365@gmail.com>
2021-05-03 15:13:10 +0900

    06

---
+++ Chapter2/_06/FullName.cs
@@ -0,0 +1,15 @@
+namespace _06
+{
+  class FullName
+  {
+    public FullName(string firstName, string lastName)
+    {
+      FirstName = firstName;
+      LastName = lastName;
+    }
+
+    public string FirstName { get; }
+    public string LastName { get; }
+
+  }
+}
---
+++ Chapter2/_06/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullname = new FullName("john", "smith");
+            Console.WriteLine(fullname.LastName);
+        }
+    }
+}
