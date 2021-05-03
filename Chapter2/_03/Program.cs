1d2fb1dda5b61b28fedb171c230a4834ce01b4c7
nuko365 <nuko365@gmail.com>
2021-05-03 14:20:54 +0900

    03

---
+++ Chapter2/_03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "john smith";
+            var tokens = fullName.Split(' ');
+            var lastNme = tokens[0];
+            Console.WriteLine(lastNme);
+        }
+    }
+}
