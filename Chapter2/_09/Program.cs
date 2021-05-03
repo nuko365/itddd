ccc1ec1316a97995b2da3c574f278d621017a154
nuko365 <nuko365@gmail.com>
2021-05-03 15:50:18 +0900

    09

---
+++ Chapter2/_09/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // "こんにちは".ChangeTo("Hello");
+            Console.WriteLine("こんにちは");
+        }
+    }
+}
