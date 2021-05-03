b859858443d16d99fd7ff0af97f77c0ee37dca46
nuko365 <nuko365@gmail.com>
2021-05-03 14:21:49 +0900

    04

---
+++ Chapter2/_04/FullName.cs
@@ -0,0 +1,15 @@
+namespace _04
+{
+  class FullName
+  {
+    public FullName(string fistName, string lastNme)
+    {
+      FirstName = firstName;
+      LastName = lastName;
+    }
+
+    public string FirstName { get; }
+    public string LastName { get; }
+
+  }
+}
