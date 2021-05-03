c04d6cc01277c38ef822c50808f4190a7bf16d71
nuko365 <nuko365@gmail.com>
2021-05-03 15:59:54 +0900

    11

---
+++ Chapter2/_11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 数字の変更
+            var num = 0;
+            num = 1;
+            Console.WriteLine(num);
+
+            // 文字の変更
+            var c = '0';
+            c = 'b';
+            Console.WriteLine(c);
+
+            // 文字列の変更
+            var greet = "こんにちは";
+            greet = "hello";
+            Console.WriteLine(greet);
+        }
+    }
+}
