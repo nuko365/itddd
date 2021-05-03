697b93ba6a123b2a0caf0cea20b5285dba2b598d
nuko365 <nuko365@gmail.com>
2021-05-03 15:56:41 +0900

    10

---
+++ Chapter2/_10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = new FullName("masanobu", "naruse");
+            Console.WriteLine(fullName.LastName);
+            fullName.ChangeLastName("sato");
+            Console.WriteLine(fullName.LastName);
+        }
+    }
+}
