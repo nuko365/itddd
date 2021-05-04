f428b17d9ca1302a34334de6b8e10b8a050c6ff0
nuko365 <nuko365@gmail.com>
2021-05-04 12:59:34 +0900

    17

---
+++ Chapter2/_17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameA = new FullName("masanobu", "naruse");
+            var nameB = new FullName("john", "smith");
+
+            var compareReuslt = nameA.Equals(nameB);
+            Console.WriteLine(compareReuslt);
+
+            var compareResult2 = nameA == nameB;
+            Console.WriteLine(compareResult2);
+        }
+    }
+}
